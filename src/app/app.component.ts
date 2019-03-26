2dcf10235030fe5d97f5dbb0c3924bf6c1a2c319
guru <guru.appnings@gmail.com>
2019-03-26 20:27:27 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  mainObject : any;
+  _jsonurl = 'http://www.mocky.io/v2/5c3c7ad13100007400a1a401';
+
+  constructor(private http:HttpClient ){
+
+    this.getJSON().subscribe(data =>{
+      this.responseData(data);
+    })
+  }
+
+  public getJSON() : Observable <any>{
+    return this.http.get(this._jsonurl);
+  }
+
+  responseData(data){
+    this.mainObject = this.getTreeStructure(data.nodes,0);
+    console.log(this.mainObject);
+  }
+
+  getTreeStructure(arr,parent){
+    var Obj =[];
+    for(var i in arr){
+      if(arr[i].parent == parent){
+        var children = this.getTreeStructure(arr,arr[i].id);
+
+        if(children){
+          arr[i]['children'] = children;
+        }
+
+        Obj.push(arr[i]);
+      }
+    }
+
+    return Obj;
+  }
+
 }
